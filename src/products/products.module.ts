fc8034dc3315a11d2ec790ddae541e9e8e37c44f
kienthu2 <kien@example.com>
2022-11-22 15:45:52 +0700

    import model into product module

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,10 +2,21 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './services/products.service';
 import { ProductsController } from './controllers/products.controller';
 import { ProductProviders } from './providers/products.providers';
+import { ProductLinesProviders } from './providers/product_lines.providers';
+import { ProductMoveProviders } from './providers/product_move.providers';
 
 @Module({
   controllers: [ProductsController],
-  providers: [ProductsService, ...ProductProviders],
-  exports: [...ProductProviders],
+  providers: [
+    ProductsService,
+    ...ProductProviders,
+    ...ProductLinesProviders,
+    ...ProductMoveProviders,
+  ],
+  exports: [
+    ...ProductProviders,
+    ...ProductLinesProviders,
+    ...ProductMoveProviders,
+  ],
 })
 export class ProductsModule {}
