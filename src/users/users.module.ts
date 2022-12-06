4e7a0fa9e086f00721691c5d94d0d3d25b2c27ac
KIEN HOANG <81230694+trungkien20020428@users.noreply.github.com>
2022-12-07 01:50:27 +0700

    update role provider

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './services/users.service';
 import { UsersController } from './controllers/users.controller';
 import { UsersProviders } from './providers/users.providers';
-import { RoleProviders } from './providers/role.providers';
 import { UserValidate } from './validate/users.validate';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UserValidate, ...UsersProviders, ...RoleProviders],
-  exports: [UsersService, ...UsersProviders, ...RoleProviders],
+  providers: [UsersService, UserValidate, ...UsersProviders],
+  exports: [UsersService, ...UsersProviders],
 })
 export class UsersModule {}
