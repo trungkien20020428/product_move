bde1f04338755cc8771a6aa3b87df41b807025b7
KIEN HOANG <81230694+trungkien20020428@users.noreply.github.com>
2022-12-23 18:07:08 +0700

    fix env ;

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -21,7 +21,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const sequelize = new Sequelize({
         dialect: 'mysql',
-        host: 'localhost',
+        host: process.env.DB_HOST,
         port: +process.env.DB_PORT,
         username: process.env.DB_USER,
         password: process.env.DB_ROOT_PASSWORD,
